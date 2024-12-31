51a674f706f175f4b2ee68864c520821afe1495a
Yomesh Gupta <yomesh.gupta@gmail.com>
2024-12-31 21:07:42 +0530

    Added editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import "./App.css";
 
+import Editor from "./components/Editor";
+
+// A simple code reviwer app
+// we will have a two column layout
+// where we can write code
+// AI will give review on our code => PR review
+
 function App() {
   return (
     <div className="flex h-full w-full items-center justify-center overflow-hidden">
-      <div className="flex flex-row h-full w-full bg-black justify-center content-center">
-        <div className="self-center text-center">
-          <h1 className="text-9xl">
-            How I started with AI as a frontend engineer?
-          </h1>
-          <a
-            href="https://youtube.com/devtoolstech"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-8xl text-green-300 hover:text-green-500 hover:underline"
-          >
-            Subscribe to devtoolstech
-          </a>
-        </div>
-      </div>
+      <Editor />
     </div>
   );
 }
